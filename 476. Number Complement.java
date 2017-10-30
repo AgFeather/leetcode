d381c9b18b8ada8087c87813a710b010973bc4fe
Dongfang Li <15542377807@163.com>
2017-10-30 19:16:50 +0900

    Create 476. Number Complement.java

---
+++ 476. Number Complement.java
@@ -0,0 +1,16 @@
+//Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+public int findComplement(int num){
+    return ~num&((Integer.highestOneBit(num)<<1)-1;
+   // highestOneBit会返回参数最高位的数值。
+}
+
+
+//找到最小大于num的位全为1的值，然后做减法
+public int findComplement(int num){
+    int i=0,j=0;
+    while(i<num){
+        i+=Math.pow(2,j);
+        j++;
+    }
+    return i-num;
+}
