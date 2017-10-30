9c7dc1023b8859af360d56975e7c693b5d75085b
Dongfang Li <15542377807@163.com>
2017-10-30 19:29:45 +0900

    Create 521. Longest Uncommon Subsequence I.java

---
+++ 501_550/521. Longest Uncommon Subsequence I.java
@@ -0,0 +1,9 @@
+//Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings. The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.
+//解题思路：注意理解uncommon subsequence的内涵，即较长string一定是较短string的uncommon subsequence
+
+
+public int findLUSlength(String a, String b) {
+        if(a.equals(b))
+            return -1;
+        return a.length()>b.length()?a.length():b.length();
+    }
