c6d9eb821b6a86c6d93981fb842fad81af531527
Dongfang Li <15542377807@163.com>
2017-11-05 18:39:05 +0900

    Update 520. Detect Capital.java

--- 501_550/520. Detect Capital.java
+++ 501_550/520. Detect Capital.java
@@ -1,15 +1,15 @@
-Given a word, you need to judge whether the usage of capitals in it is right or not.
+// Given a word, you need to judge whether the usage of capitals in it is right or not.
 
-We define the usage of capitals in a word to be right when one of the following cases holds:
+// We define the usage of capitals in a word to be right when one of the following cases holds:
 
-All letters in this word are capitals, like "USA".
-All letters in this word are not capitals, like "leetcode".
-Only the first letter in this word is capital if it has more than one letter, like "Google".
-Otherwise, we define that this word doesn't use capitals in a right way.
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital if it has more than one letter, like "Google".
+// Otherwise, we define that this word doesn't use capitals in a right way.
 
 
 
-很简单的题，对三种情况分别判断即可
+// 很简单的题，对三种情况分别判断即可
 
 
    public boolean detectCapitalUse(String word) {
@@ -27,3 +27,8 @@ Otherwise, we define that this word doesn't use capitals in a right way.
 		}
         return true;
   }
+
+
+//更便捷的解法，正则表达式
+
+return word.match("[A-Z]+|[a-z]+|[A-Z][a-z]+");
